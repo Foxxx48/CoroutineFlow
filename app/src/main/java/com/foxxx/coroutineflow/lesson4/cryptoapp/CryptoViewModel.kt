940f4b92f09434666475cd6e6db3a31a56eac1ab
Foxxx48 <Foxxdim@mail.ru>
2023-07-17 10:52:59 +0300

    Realisation of CryptoApp using liveData Lesson5

--- app/src/main/java/com/foxxx/coroutineflow/lesson4/cryptoapp/CryptoViewModel.kt
+++ app/src/main/java/com/foxxx/coroutineflow/lesson4/cryptoapp/CryptoViewModel.kt
@@ -1,38 +1,25 @@
 package com.foxxx.coroutineflow.lesson4.cryptoapp
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.asLiveData
 import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 
 class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
 
-    private val _state = MutableLiveData<CryptoState>(CryptoState.Initial)
-    val state: LiveData<CryptoState> = _state
-
-    init {
-        loadData()
-    }
-
-    private fun loadData() {
+    val state =
         repository.getCurrencyListFlow()
-            .onStart {
-                val currentState = _state.value
-                if (currentState !is CryptoState.Content || currentState.cryptoCurrencyList.isEmpty()) {
-                    _state.value = CryptoState.Loading
-                }
-            }
             .filter { it.isNotEmpty() }
-            .onEach {
-                _state.value = CryptoState.Content(cryptoCurrencyList = it)
+            .map { CryptoState.Content(cryptoCurrencyList = it) as CryptoState }
+            .onStart {
+                CryptoState.Loading
             }
-            .launchIn(viewModelScope)
+            .asLiveData()
 
-    }
 }
+
+
+
