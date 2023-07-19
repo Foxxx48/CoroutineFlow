830e55f030da49476c993b2c9d8600ed4e3c1fd2
Foxxx48 <Foxxdim@mail.ru>
2023-07-19 14:58:55 +0300

    TeamScore App with MutableStateFlow Lesson12

--- app/src/main/java/com/foxxx/coroutineflow/lesson12/TeamScoreViewModel.kt
+++ app/src/main/java/com/foxxx/coroutineflow/lesson12/TeamScoreViewModel.kt
@@ -1,67 +1,54 @@
 package com.foxxx.coroutineflow.lesson12
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class TeamScoreViewModel : ViewModel() {
 
-    private var cachedState: TeamScoreState = TeamScoreState.Game(0, 0)
-
-    private val _stateSharedFlow = MutableSharedFlow<TeamScoreState>(replay = 1)
-    val stateSharedFlow = _stateSharedFlow.asSharedFlow()
-        .onEach { cachedState = it }
+    private val _stateSharedFlow = MutableStateFlow<TeamScoreState>(TeamScoreState.Game(0, 0))
+    val stateSharedFlow = _stateSharedFlow.asStateFlow()
 
     fun increaseScoreWithFlow(team: Team) {
-        viewModelScope.launch {
-            if (cachedState is TeamScoreState.Game) {
-                val currentState = cachedState as TeamScoreState.Game
-                if (team == Team.TEAM_1) {
-                    val oldValue = currentState.score1
-                    val newValue = oldValue + 1
-                    _stateSharedFlow.emit(
-                        currentState.copy(
+        val currentState = _stateSharedFlow.value
+        if (currentState is TeamScoreState.Game) {
+            if (team == Team.TEAM_1) {
+                val oldValue = currentState.score1
+                val newValue = oldValue + 1
+                _stateSharedFlow.value =
+                    currentState.copy(
+                        score1 = newValue,
+                        score2 = currentState.score2
+                    )
+
+                if (newValue >= WINNER_SCORE) {
+                    _stateSharedFlow.value =
+                        TeamScoreState.Winner(
+                            winnerTeam = Team.TEAM_1,
                             score1 = newValue,
                             score2 = currentState.score2
                         )
+                }
+            } else {
+                val oldValue = currentState.score2
+                val newValue = oldValue + 1
+
+                _stateSharedFlow.value =
+                    currentState.copy(
+                        score1 = currentState.score1,
+                        score2 = newValue
                     )
 
-                    if (newValue >= WINNER_SCORE) {
-                        _stateSharedFlow.emit(
-                            TeamScoreState.Winner(
-                                winnerTeam = Team.TEAM_1,
-                                score1 = newValue,
-                                score2 = currentState.score2
-                            )
-                        )
-                    }
-                } else {
-                    val oldValue = currentState.score2
-                    val newValue = oldValue + 1
-
-                    _stateSharedFlow.emit(
-                        currentState.copy(
+                if (newValue >= WINNER_SCORE) {
+                    _stateSharedFlow.value =
+                        TeamScoreState.Winner(
+                            winnerTeam = Team.TEAM_2,
                             score1 = currentState.score1,
                             score2 = newValue
                         )
-                    )
-
-                    if (newValue >= WINNER_SCORE) {
-                        _stateSharedFlow.emit(
-                            TeamScoreState.Winner(
-                                winnerTeam = Team.TEAM_2,
-                                score1 = currentState.score1,
-                                score2 = newValue
-                            )
-                        )
-                    }
                 }
             }
         }
-
     }
 
     companion object {
